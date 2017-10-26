6de866fa97ec4825bd60a5177c9bdd66962a2204
BNA22 <BNA22>
2017-10-26 14:30:14 +0000

    classe

---
+++ src/main/java/app/NomeDaClasse.java
@@ -0,0 +1,21 @@
+package app;
+
+
+/**
+ * Classe que representa ...
+ * 
+ * @author Beatriz Alves
+ * @version 1.0
+ * @since 2017-10-26
+ *
+ */
+ 
+public class NomeDaClasse {
+
+	/**
+	 * Construtor
+	 **/
+	public NomeDaClasse (){
+	}
+
+}
